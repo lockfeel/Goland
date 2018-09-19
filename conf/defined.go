80e881084d1da45e93640111b1e4fe02f13c9e48
张文晓 <837045534@qq.com>
2018-09-19 15:04:35 +0800

    修改地址

--- conf/defined.go
+++ conf/defined.go
@@ -1,8 +1,8 @@
 package conf
 
 /**配置文件*/
-const MONGOURL  = "mongodb://root:Jeepcn123@47.94.41.43:3717"
-const MYSQLURL  = "root:root@tcp(47.95.36.78:3306)/jeepadmin?charset=utf8"
-const MONGOPRO  = "mongodb://root:Jeepcn123@dds-2ze940bdbf0199e41.mongodb.rds.aliyuncs.com:3717"
-const MONGOAIR  = "mongodb://root:Jeepcn123@dds-2ze940bdbf0199e42.mongodb.rds.aliyuncs.com:3717"
-const ROUTERJS  = "/Users/zhangwenxiao/go/src/xiaomatv.cn/conf/router.json"
+const MONGOURL  = "mongodb://"
+const MYSQLURL  = "mysql@"
+const MONGOPRO  = "mongodb://"
+const MONGOAIR  = "mongodb://"
+const ROUTERJS  = "../conf/router.json"
